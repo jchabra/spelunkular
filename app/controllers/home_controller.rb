558d7a020474a0fc8e9ebf106fb8f8d5a44d10b5
Tim LaTorre <timlatorre@gmail.com>
2012-12-19 16:48:37 -0500

    home controller, update routes, index.html.haml

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,20 @@
 class HomeController < ApplicationController
+
   def index
-    
   end
+
+  def get_html
+    url = params[:url]
+    @html = HTTParty.get(url)
+
+    img_tags = /<img.*?['"]([^'"]*)/m
+    a_tags = /<a .*?href.*?['"]([^'"]*)['"].*?>\w*<\/a>/m
+
+    @img_result = @html.scan(img_tags).flatten
+    @a_result = @html.scan(a_tags).flatten
+
+    binding.pry
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Spelunkular::Application.routes.draw do
 
-
+  root :to => 'home#index'
+  post '/get_html' => 'home#get_html'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +52,7 @@ Spelunkular::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+
 
   # See how all your routes lay out with "rake routes"
 
