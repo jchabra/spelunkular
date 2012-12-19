512808a8256126b390f288904f6bc63b802c3f59
Tim LaTorre <timlatorre@gmail.com>
2012-12-19 17:37:10 -0500

    get_html.js.erb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,13 +7,13 @@ class HomeController < ApplicationController
     url = params[:url]
     @html = HTTParty.get(url)
 
-    img_tags = /<img.*?['"]([^'"]*)/m
-    a_tags = /<a .*?href.*?['"]([^'"]*)['"].*?>\w*<\/a>/m
+    images = /<img.*?['"]([^'"]*)/m
+    links = /<a .*?href.*?['"]([^'"]*)['"].*?>\w*<\/a>/m
 
-    @img_result = @html.scan(img_tags).flatten
-    @a_result = @html.scan(a_tags).flatten
+    @images = @html.scan(images).flatten
+    @links = @html.scan(links).flatten
 
-    binding.pry
+
   end
 
 
